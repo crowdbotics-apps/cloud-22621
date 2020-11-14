41de117c78fb62b6bb7876fa280e9f7d8502ea2b
Crowdbotics <team@crowdbotics.com>
2020-11-14 16:22:46 +0000

    Update models

--- cloud_22621/wsgi.py
+++ cloud_22621/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cloud_22621.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cloud_22621.settings")
 
 application = get_wsgi_application()
